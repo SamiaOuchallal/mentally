852375240bfb379f76bf7b8454385c9a13eac0ff
olivialitou-creator <olivia.litou@gmail.com>
2026-04-24 00:00:48 +0200

    modif test

--- letest.dart
+++ letest.dart
@@ -1,1 +1,14 @@
 import 'package:flutter/material.dart';
+void main() => runApp(racineApp());
+
+class racineApp extends StatelessWidget {
+    @override 
+    Widget build(BuildContext context) {
+        return MaterialApp(
+            home:HomePage()
+
+        ); //fin MaterialApp
+
+    }//fin build
+
+}// fin class racineApp
