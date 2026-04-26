cf7a6208311e6a94542db652ddf6f52bcda4129d
olivialitou-creator <olivia.litou@gmail.com>
2026-04-26 14:15:04 +0200

    ajout des bibliothèque et de la page

---
+++ page1_humeurdujour.dart
@@ -0,0 +1,9 @@
+// page 1 : entrée de l'humeur du jour en choisissant un emoji + possibilité d'entrer du texte des iamges
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+
+class Page1 extends StatefulWidget {
+
+}// fin Page1
